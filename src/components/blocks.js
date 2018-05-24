d8f77f1fff4bd9a75b19d25a31eaf1edc27fce7d
Randy <rrw.burnet@gmial.com>
2018-05-24 10:34:55 +0200

    first commit

---
+++ src/components/blocks.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Fade } from 'react-reveal';
+
+const generateBlocks = ({blocks}) => {
+  if(blocks){
+    return blocks.map((item)=>{
+      return(
+        <Fade key={item.id} bottom className={`item ${item.type}`}>
+          <div className="veil"></div>
+          <div 
+            className="image"
+            style={{background:`url(/images/blocks/${item.image}) no-repeat`}}
+            >
+          </div>
+          <div className="title">
+            <Link to={item.link}> {item.title}</Link>
+          </div>
+        </Fade>
+      )
+    })
+
+  }
+}
+
+const Blocks = (props) => {
+
+  return(
+    <div className="home_blocks">
+      {generateBlocks(props)}
+    </div>
+  )
+}
+
+export default Blocks
